ee705ea1cbea9ac3db5ce478777be4c41421ebaf
Abhishek <takawaneabhishek04@gmail.com>
2022-10-04 15:26:00 +0530

    first commit

---
+++ file.js
@@ -0,0 +1,32 @@
+var x =document.getElementById("a").innerHTML = "Operators are used in console";
+
+let a = 60;
+let b= 40;
+let c = a+b;
+console.log( "Addition of 60 and 40  ="+c);
+
+let f= a-b;
+console.log("Substraction of 60 and 40 ="+f)
+
+let i = a/b
+console.log("Division of 60 and 40 ="+i)
+
+let l= a * b
+console.log("Multi. of 60 and 40 = "+l)
+
+let o = a % b
+console.log("Reminder of 60 and 40 ="+o)
+console.log("Use of > operator")
+console.log( 60>40)
+console.log("Use of && operator")
+console.log(60>40 && 40<60)
+console.log("Use of || operator")
+console.log(60>40 || 40>60)
+console.log("Preincrement and predecrement of a")
+console.log(++a);
+console.log(--a);
+console.log("Postincrement and Postdecrement of a")
+console.log(a++);
+console.log(a--);
+
+
